2dcd8e2fa9c4219d42a3f83d017a24ddaad77fe7
Eden Nahum <Edenik5@gmail.com>
2021-01-13 15:33:49 +0200

    Login Register UI  fixes

--- src/public/scripts/auth.js
+++ src/public/scripts/auth.js
@@ -9,50 +9,51 @@ const init = (title) => {
     }
 }
 
+const emailInput = ` 
+    <div class="form-group  mb-4">
+    <label for="emailInput">Email</label>
+    <input type="email" id="emailInput" class="form-control" placeholder="Email address">
+    </div>`;
+
+const passwordInput = `
+    <div class="form-group mb-4">
+    <label for="passwordInput">Password</label>
+    <input type="password" id="passwordInput" class="form-control" placeholder="***********">
+    </div>`;
+
+const repeatPasswordInput = `
+    <div class="form-group mb-4">
+    <label for="repeatPasswordInput">Repeat Password</label>
+    <input type="password" id="repeatPasswordInput" class="form-control" placeholder="***********">
+    </div>`;
+
+const notifiers = `
+    <p id="error"></p>
+    <p id="result"></p>`;
+
 const createLoginForm = () => {
     const form = `
-        <div class="form-group  mb-4">
-            <label for="email">Email</label>
-            <input type="email" id="email" class="form-control" placeholder="Email address">
-        </div>
-        <div class="form-group mb-4">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" class="form-control" placeholder="***********">
-        </div>
-        <p id="error"></p>
-        <p id="result"></p>
+        ${emailInput}
+        ${passwordInput}
+        ${notifiers}
         <button class="btn btn-block login-btn mb-4"> Login </button>
-        <p>Don't have an account? <a href="/auth/register" class="text-reset">Register here</a></p>
-        </div>`;
+        <p>Don't have an account? <a href="/auth/register" class="text-reset">Register here</a></p>`;
 
     setFormOnPage(form);
 }
 
 const createRegisterForm = () => {
     const form = `
-        <div class="form-group  mb-4">
-            <label for="emailInput">Email</label>
-            <input type="email" id="emailInput" class="form-control" placeholder="Email address">
-        </div>
-        <div class="form-group mb-4">
-            <label for="passwordInput">Password</label>
-            <input id="passwordInput" type="password" id="password" class="form-control" placeholder="***********">
-        </div>
-
-        <div class="form-group mb-4">
-            <label for="repeatPasswordInput">Repeat Password</label>
-            <input type="password" id="repeatPasswordInput" class="form-control" placeholder="***********">
-        </div>
+        ${emailInput}
+        ${passwordInput}
+        ${repeatPasswordInput}
+        ${notifiers}
         <p id="generatedPassword"></p>
-        <p id="error"></p>
-        <p id="result"></p>
         <button class="btn btn-block login-btn mb-4"> Register </button>
-        <button onclick="generatedPasswordHandler()"  class="btn btn-block login-btn mb-4"> Generate Password </button>
-        <p>Already have an account? <a href="/auth/login" class="text-reset">Login here</a></p>
-        </div>`;
+        <button onclick="generatedPasswordHandler()" class="btn btn-block login-btn mb-4"> Generate Password </button>
+        <p>Already have an account? <a href="/auth/login" class="text-reset">Login here</a></p>`;
 
     setFormOnPage(form);
-
 }
 
 const setFormOnPage = (form) => {
@@ -68,6 +69,8 @@ const loginHandler = () => {
 const registerHandler = () => {
 
 }
+
+let generatedPasswordTimeOut;
 
 const generatedPasswordHandler = () => {
     fetch(`${url}/users/generatePassword`, { method: "GET" })
@@ -87,7 +90,8 @@ const setGeneratedPassOnPage = (password) => {
     <p>* Password will disappear after 10 sec.</p>
     `
 
-    setTimeout(() => {
+    clearTimeout(generatedPasswordTimeOut);
+    generatedPasswordTimeOut = setTimeout(() => {
         element.innerHTML = ``;
     }, 10000);
 }
